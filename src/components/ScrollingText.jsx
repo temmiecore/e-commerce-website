eab63365821b1eb4f6f0941d273720f847bb6f93
Nikita Horobets <cloudedlegends1@gmail.com>
2024-04-03 16:14:39 +0200

    Add infinite scrolling text

---
+++ src/components/ScrollingText.jsx
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import "./styles/ScrollingText.css"
+
+function ScrollingText({ children, speed }) {
+    return <div className={"scrolling-container " + speed}>
+        <h1 className="heading-text big scrolling-text">
+            {children}
+        </h1>
+        <h1 className="heading-text big scrolling-text">
+            {children}
+        </h1>
+        <h1 className="heading-text big scrolling-text">
+            {children}
+        </h1>
+        <h1 className="heading-text big scrolling-text">
+            {children}
+        </h1>
+    </div>
+}
+
+ScrollingText.propTypes = {
+    children: PropTypes.string.isRequired,
+    speed: PropTypes.string.isRequired,
+};
+
+export default ScrollingText;
+
