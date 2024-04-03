d2000d7590c7ffeb1afcb12b18741c467b2881d1
Nikita Horobets <cloudedlegends1@gmail.com>
2024-04-03 16:13:13 +0200

    Add prop validation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
+import PropTypes from 'prop-types'
 import "./styles/Button.css"
 
 function Button({ children }) {
     return <button className="body-text">{children}</button>
 }
+
+Button.propTypes = {
+    children: PropTypes.node.isRequired,
+};
 
 export default Button;
