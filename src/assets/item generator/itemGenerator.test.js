6cb36b23c8af55dde1fb656de3f28b46eb631cac
Nikita Horobets <cloudedlegends1@gmail.com>
2024-04-04 14:35:19 +0200

    test: add itemGenerator tests

---
+++ src/assets/item generator/itemGenerator.test.js
@@ -0,0 +1,27 @@
+import { expect, it } from "vitest";
+import { getItemName, getItemPrice, getItemImage, getItem } from "./itemGenerator"
+
+describe('getItem functions', () => {
+    it('should return string for Name/Image and number for Price', async () => {
+        const itemName = await getItemName();
+        expect(typeof itemName).toBe("string");
+
+        const itemPrice = await getItemPrice();
+        expect(typeof itemPrice).toBe("number");
+        
+        const itemImage = await getItemImage();
+        expect(typeof itemImage).toBe("string");
+    });
+});
+
+describe('getItem', () => {
+    it('should handle async correctly', async () => {
+        const data = await getItem();
+        expect(data).toBeDefined();
+    });
+    it('should return array of three items', async () => {
+        const data = await getItem();
+        expect(Array.isArray(data)).toBe(true);
+        expect(data.length).toBe(3);
+    });
+})
