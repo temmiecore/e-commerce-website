c88a023fb1d113d9b6dfbec30e6a76380ed18e7a
Nikita Horobets <cloudedlegends1@gmail.com>
2024-04-21 16:34:35 +0200

    test: add Checkout tests

---
+++ src/__tests__/Checkout.test.jsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react"
+import Checkout from "../components/Checkout";
+import { expect, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
+
+describe('Checkout', () => {
+    const cartItems = [
+        { id: 1, name: "Item1", price: "100", image: "", qty: 1 },
+        { id: 2, name: "Item2", price: "200", image: "", qty: 1 },
+        { id: 3, name: "Item3", price: "200", image: "", qty: 1 },
+    ];
+
+    it('should calculate correct price based on cartItems', () => {
+        const handleCheckoutMock = vi.fn();
+
+        render(<Checkout cartItems={cartItems} handleCheckout={handleCheckoutMock}/>)
+
+        expect(screen.getByText("Subtotal: $500")).toBeInTheDocument();
+        expect(screen.getByText("Fees: $10")).toBeInTheDocument();
+        expect(screen.getByText("Tax: $25")).toBeInTheDocument();
+        expect(screen.getByText("Total: $535")).toBeInTheDocument();
+
+        screen.debug();
+    })
+
+    it('should call handleCheckout on button click', async () => {
+        const handleCheckoutMock = vi.fn();
+        const user = userEvent.setup();
+        
+        render(<Checkout cartItems={cartItems} handleCheckout={handleCheckoutMock}/>)
+
+        await user.click(screen.getByText("Checkout now"));
+
+        expect(handleCheckoutMock).toHaveBeenCalledOnce();
+    })
+})
--- src/__tests__/Nav.test.jsx
+++ src/__tests__/Nav.test.jsx
@@ -2,16 +2,13 @@ import { render, screen } from "@testing-library/react"
 import Nav from "../components/Nav";
 import { MemoryRouter } from "react-router-dom";
 import { act } from "react-dom/test-utils";
-import { beforeEach, expect } from "vitest";
+import { expect } from "vitest";
 
-beforeEach(() => {
-    render(<MemoryRouter><Nav/></MemoryRouter>);
-    screen.debug();
-});
-
-// test routing here or higher?
 describe("Nav", () => {
     it("should render images at width lower than 480px", () => {
+        render(<MemoryRouter><Nav/></MemoryRouter>);
+        screen.debug();
+
         act(() => {
             window.innerWidth = 479;
             window.dispatchEvent(new Event("resize"));
@@ -25,6 +22,8 @@ describe("Nav", () => {
 
     });
     it("should render headings at width higher than 480px", () => {
+        render(<MemoryRouter><Nav/></MemoryRouter>);
+    
         act(() => {
             window.innerWidth = 481;
             window.dispatchEvent(new Event("resize"));
