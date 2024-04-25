df92fc00a58ee29e1f0174dda2f94f58021d4e8b
Nikita Horobets <cloudedlegends1@gmail.com>
2024-04-25 17:21:38 +0200

    Import clothing images to itemGenerator

--- src/assets/item generator/itemGenerator.js
+++ src/assets/item generator/itemGenerator.js
@@ -1,3 +1,34 @@
+import image1 from "./clothing images/clothimage1.jpg";
+import image2 from "./clothing images/clothimage2.jpg";
+import image3 from "./clothing images/clothimage3.jpg";
+import image4 from "./clothing images/clothimage4.jpg";
+import image5 from "./clothing images/clothimage5.jpg";
+import image6 from "./clothing images/clothimage6.jpg";
+import image7 from "./clothing images/clothimage7.jpg";
+import image8 from "./clothing images/clothimage8.jpg";
+import image9 from "./clothing images/clothimage9.jpg";
+import image10 from "./clothing images/clothimage10.jpg";
+import image11 from "./clothing images/clothimage11.jpg";
+import image12 from "./clothing images/clothimage12.jpg";
+import image13 from "./clothing images/clothimage13.jpg";
+
+const itemImages = [];
+itemImages.push(
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13
+);
+
 const itemAdjectives = [
   "Baggy",
   "Casual",
@@ -16,26 +47,26 @@ const itemAdjectives = [
 ];
 
 const itemNames = [
-    "T-Shirt",
-    "Cardigan", 
-    "Shirt", 
-    "Corset", 
-    "Crop Top", 
-    "Duster", 
-    "Hoodie", 
-    "Tank Top", 
-    "Turtleneck", 
-    "Polo Shirt", 
-    "Blouse", 
-    "Sweater", 
-    "V-Neck", 
-    "Vest", 
-    "Cargo Pants", 
-    "Jeans", 
-    "Khakis", 
-    "Leggins", 
-    "Sweatpants", 
-    "Shorts",
+  "T-Shirt",
+  "Cardigan",
+  "Shirt",
+  "Corset",
+  "Crop Top",
+  "Duster",
+  "Hoodie",
+  "Tank Top",
+  "Turtleneck",
+  "Polo Shirt",
+  "Blouse",
+  "Sweater",
+  "V-Neck",
+  "Vest",
+  "Cargo Pants",
+  "Jeans",
+  "Khakis",
+  "Leggins",
+  "Sweatpants",
+  "Shorts",
 ];
 
 const itemColors = [
@@ -62,10 +93,12 @@ const itemColors = [
 function getItemName() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const itemName = 
-      itemAdjectives[Math.floor(Math.random() * itemAdjectives.length)] + " " +
-      itemNames[Math.floor(Math.random() * itemNames.length)] + ", " +
-      itemColors[Math.floor(Math.random() * itemColors.length)];
+      const itemName =
+        itemAdjectives[Math.floor(Math.random() * itemAdjectives.length)] +
+        " " +
+        itemNames[Math.floor(Math.random() * itemNames.length)] +
+        ", " +
+        itemColors[Math.floor(Math.random() * itemColors.length)];
       resolve(itemName);
     }, Math.floor(Math.random() * 300));
   });
@@ -81,19 +114,19 @@ function getItemPrice() {
 }
 
 function getItemImage() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        const itemImage = "src/assets/item generator/clothing images/clothimage" + (Math.floor(Math.random() * 13) + 1) + ".jpg";
-        resolve(itemImage);
-      }, Math.floor(Math.random() * 600));
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const itemImage = itemImages[Math.floor(Math.random() * 13)];
+      resolve(itemImage);
+    }, Math.floor(Math.random() * 600));
+  });
 }
 
 async function getItem() {
-    const name = getItemName();
-    const price = getItemPrice();
-    const image = getItemImage();
-    return Promise.all([name, price, image]);
+  const name = getItemName();
+  const price = getItemPrice();
+  const image = getItemImage();
+  return Promise.all([name, price, image]);
 }
 
 export { getItemName, getItemPrice, getItemImage, getItem };
